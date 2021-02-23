f29050dd675d14cea1d9a78d325bb6309a306256
Josh Eskew <jeskew82@gmail.com>
2021-02-23 13:11:41 -0500

    finished script. Everything works well and data is being saved to local storage

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
+// This saves selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 // This updates count and total
-function changeSelectedCount() {
+function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatCount = selectedSeats.length;
 
@@ -19,18 +27,41 @@ function changeSelectedCount() {
 
     count.innerText = selectedSeatCount;
     total.innerText = selectedSeatCount * ticketPrice;
+};
+
+// Populates the UI with data form local storage on page load
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
-// Mpvie click event
+// Movie click event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
-    changeSelectedCount();
-})
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+});
 
 // Seat click event
 container.addEventListener('click', e => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
-        changeSelectedCount();
+        updateSelectedCount();
     }
-})
+});
+
+// This updates the total and the count
+updateSelectedCount();
